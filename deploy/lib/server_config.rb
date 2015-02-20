ff228fd83b9acdc86884abff6ef96f141c9cf320
Geert Josten <geert.josten@marklogic.com>
2015-02-20 10:52:39 +0100

    First attempt to fix #365

--- deploy/lib/server_config.rb
+++ deploy/lib/server_config.rb
@@ -24,6 +24,9 @@ require 'MLClient'
 require 'date'
 require 'ml_rest'
 
+optional_require 'open-uri'
+optional_require 'nokogiri'
+
 class ExitException < Exception; end
 
 class HelpException < Exception
@@ -424,6 +427,54 @@ but --no-prompt parameter prevents prompting for password. Assuming 8.'
     end
   end
 
+  def self.howto
+    begin
+      search = ARGV.first
+      doc = Nokogiri::HTML(open('http://github.com/marklogic/roxy/wiki/_pages'))
+
+      pages = doc.css('.content').select do |page|
+        search == nil or page.text.downcase().include? search
+      end
+
+      selected = 1
+
+      if pages.length > 1
+        count = 0
+        pages.each do |page|
+          count = count + 1
+          puts "#{count} - #{page.text}\n\thttps://github.com/#{page.xpath('a/@href').text}"
+        end
+  
+        print "Select a page: "
+        selected = STDIN.gets.chomp().to_i
+        if selected == 0
+          exit
+        end
+      
+        if selected > pages.length
+          selected = pages.length
+        end
+      end
+
+      count = 0
+      pages.each do |page|
+        count = count + 1
+        if count == selected
+    
+          puts "\n#{page.text}\n\thttps://github.com/#{page.xpath('a/@href').text}"
+    
+          uri = "https://github.com/#{page.xpath('a/@href').text}"
+          doc = Nokogiri::HTML(open(uri))
+
+          puts doc.css('.markdown-body').text.gsub(/\n\n\n+/, "\n\n")
+    
+        end
+      end
+    rescue NameError => e
+      puts "Missing library: #{e}"
+    end
+  end
+  
   def execute_query(query, properties = {})
     r = nil
     if @server_version == 4
